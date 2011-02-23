ddf772b226d1d54ef3e3dae49ee63cb9d26329b4
Arco Overbeek <arcooverbeek@gmail.com>
2011-02-23 02:08:35 +0100

    added live loading.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {  
   $('#next').live('inview', function(event, visible) {
     if (visible) {
-      if ($(this).hasClass('loaded') == false) {
-        var url = $(this).attr("href");
-        $.get(url, function(data) {
-          $('#rows').append(data);
-          alert('Load was performed.');
-        });
-        $(this).addClass('loaded');
-      }
+      $('#loader').show();
+      var url = $(this).attr("href");
+      $.get(url, function(data) {
+        $('#next').addClass('loaded');
+        $('#rows').append(data);
+        $('#loader').hide();
+      });
+      $('.loaded').remove();
     }
   });
 });
