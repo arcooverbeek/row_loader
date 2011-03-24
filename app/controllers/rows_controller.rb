3f9520ab3f6900be8e6b2e7aadd92e0851c7380e
Arco Overbeek <arcooverbeek@gmail.com>
2011-03-24 12:43:05 +0100

    Added a status, rownumbers and cleaned up the code.

--- app/controllers/rows_controller.rb
+++ app/controllers/rows_controller.rb
@@ -2,11 +2,14 @@ class RowsController < ApplicationController
 
   def index
     @rows = Row.limit(50)
+    @end = 50
   end
   
   def load_more
-    if params[:start] && params[:end]
-      @rows = Row.limit(50)  
+    if params[:end]
+      @rows = Row.limit(50)
+      #@rows = Row.find(:all, :conditions => { :grade => 9..12 })
+      @end = params[:end].to_i + 50  
     end
     render :layout => false  
   end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,14 +1,13 @@
 $(document).ready(function() {  
   $('#next').live('inview', function(event, visible) {
     if (visible) {
-      $('#loader').show();
       var url = $(this).attr("href");
-      $.get(url, function(data) {
-        $('#next').addClass('loaded');
+      $(this).remove();
+      $('#status').html("Loading..");
+      $.get(url, function(data) { 
         $('.rowlist').append(data);
-        $('#loader').hide();
+        $('#status').html("Idle");
       });
-      $('.loaded').remove();
     }
   });
 });
